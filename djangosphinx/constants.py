0d8f546a30194a9c3a9a64a74b06aab0a6a415b4
igorn <igorn@freshlimestudio.com>
2012-01-08 13:29:53 +0200

    fix api version

--- djangosphinx/constants.py
+++ djangosphinx/constants.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 __all__ = ('SPHINX_API_VERSION',)
 
+# 0x116
 # 0x113 = 1.19
 # 0x107 = 1.17
-SPHINX_API_VERSION = getattr(settings, 'SPHINX_API_VERSION', 0x107)
+SPHINX_API_VERSION = getattr(settings, 'SPHINX_API_VERSION', 0x116)
